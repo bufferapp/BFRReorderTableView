c0c23dc4cc2f2caf6b832d4558616c767037b2df
Jordan Morgan <jordan@dreaminginbinary.co>
2016-09-15 16:30:11 -0500

    Added toggle to test multiple sections and single in demo
    Need to fix: spacerRow logic
                        exception with using sections

--- BFRTableReorder/BFRTableReorder/ViewController.m
+++ BFRTableReorder/BFRTableReorder/ViewController.m
@@ -11,8 +11,9 @@
 
 @interface ViewController () <UITableViewDelegate, UITableViewDataSource, BFRTableViewReorderDelegate>
 
-//@property (strong, nonatomic) NSMutableArray <NSMutableArray<NSString *> *> *items;
 @property (strong, nonatomic) NSMutableArray <NSString *> *items;
+@property (strong, nonatomic) NSMutableArray <NSMutableArray<NSString *> *> *multipleItems;
+@property (nonatomic) BOOL useMultipleSections;
 
 @end
 
@@ -20,11 +21,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.useMultipleSections = YES;
+    self.items = [[NSMutableArray alloc] initWithArray:@[@"1", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"10"]];
     
-    //NSMutableArray *section1 = [[NSMutableArray alloc] initWithObjects:@"1", @"2", nil];
-    //NSMutableArray *section2 = [[NSMutableArray alloc] initWithObjects:@"3", @"4", nil];
-    //NSMutableArray *section3 = [[NSMutableArray alloc] initWithObjects:@"5", @"6", nil];
-    self.items = [[NSMutableArray alloc] initWithArray:@[@"1", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"10"/*, section2, section3*/]];
+    NSMutableArray *section1 = [[NSMutableArray alloc] initWithObjects:@"1", @"2", nil];
+    NSMutableArray *section2 = [[NSMutableArray alloc] initWithObjects:@"3", @"4", nil];
+    NSMutableArray *section3 = [[NSMutableArray alloc] initWithObjects:@"5", @"6", nil];
+    self.multipleItems = [[NSMutableArray alloc] initWithArray:@[section1, section2, section3]];
     
     UITableView *tv = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStyleGrouped];
     [tv registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
@@ -43,21 +46,25 @@
 
 #pragma mark - Reordering
 - (void)tableView:(UITableView *)tableView redorderRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-    //NSString *item = self.items[fromIndexPath.section][fromIndexPath.row];
-    //[self.items[fromIndexPath.section] removeObjectAtIndex:fromIndexPath.row];
-    //[self.items[toIndexPath.section] insertObject:item atIndex:toIndexPath.row];
-    NSString *item = self.items[fromIndexPath.row];
-    [self.items removeObjectAtIndex:fromIndexPath.row];
-    [self.items insertObject:item atIndex:toIndexPath.row];
+    
+    if (self.useMultipleSections) {
+        NSString *item = self.multipleItems[fromIndexPath.section][fromIndexPath.row];
+        [self.multipleItems[fromIndexPath.section] removeObjectAtIndex:fromIndexPath.row];
+        [self.multipleItems[toIndexPath.section] insertObject:item atIndex:toIndexPath.row];
+    } else {
+        NSString *item = self.items[fromIndexPath.row];
+        [self.items removeObjectAtIndex:fromIndexPath.row];
+        [self.items insertObject:item atIndex:toIndexPath.row];
+    }
 }
 
 #pragma mark - Tableview
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 10;
+    return self.useMultipleSections ? 2 : 10;
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
+    return self.useMultipleSections ? 3 : 1;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -67,8 +74,13 @@
     }
     
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
-    //cell.textLabel.text = self.items[indexPath.section][indexPath.row];
-    cell.textLabel.text = self.items[indexPath.row];
+
+    if (self.useMultipleSections) {
+        cell.textLabel.text = self.multipleItems[indexPath.section][indexPath.row];
+    } else {
+        cell.textLabel.text = self.items[indexPath.row];
+    }
+    
     return cell;
 }
 
